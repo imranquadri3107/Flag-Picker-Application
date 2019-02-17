06ca682be95a65541ff67c5371f82048907f96c0
imranquadri3107 <imranquadri77@gmail.com>
2019-02-16 20:19:22 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import continents from './continents.json';
+import Continent from './Continent';
 
+//var data = continents;
+//console.log(data[0].continent);
 class App extends Component {
+  
+ 
+
+ getData = (data)=>{
+   data.prevent.default();
+  
+  console.log(data);
+ }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      
+      <div className = 'App'>
+        <h1>Flag Picker</h1><br/>
+        <h3>This App will help you to learn flags around the world in 3 Steps</h3>
+
+        <Continent continents = {continents}/>
       </div>
     );
   }
